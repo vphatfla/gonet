1cc7045f82f652265d24ce4b555677130e9fa552
van-xps-ubuntu <pvphat25@gmail.com>
2025-04-21 15:04:28 -0400

    clean main.go

--- README.md
+++ README.md
@@ -19,22 +19,19 @@ Must compile and run the binary as root
 Example running scan against server outlook.com
 
 ```
-go build && sudo ./gonet
-2025/04/17 14:10:12 Welcome to go net
-Enter IP4 address 52.96.229.242
-2025/04/17 14:10:13 Start scanning all port from 0 to 65535
-2025/04/17 14:10:16 Timeout after 3 seconds
-2025/04/17 14:10:16 Port 25(smtp) status OPEN ---Scan takes 186.170542ms
-2025/04/17 14:10:16 Port 80(http) status OPEN ---Scan takes 185.048165ms
-2025/04/17 14:10:16 Port 110(pop3) status OPEN ---Scan takes 217.369248ms
-2025/04/17 14:10:16 Port 143(imap) status OPEN ---Scan takes 216.72778ms
-2025/04/17 14:10:16 Port 443(https) status OPEN ---Scan takes 141.915058ms
-2025/04/17 14:10:16 Port 993(imaps) status OPEN ---Scan takes 330.977721ms
-2025/04/17 14:10:16 Port 995(pop3s) status OPEN ---Scan takes 330.954008ms
-2025/04/17 14:10:16 Port 587(submission) status OPEN ---Scan takes 679.338655ms
-2025/04/17 14:10:16 Closing scanner
-
+sudo gonet -i 52.96.91.34 -wkn
+Scanning well-knowns ports [0...1023]
+2025/04/21 15:03:16 Timeout after 3 seconds
+Port 25(smtp) --> Status OPEN ---  Scan takes 110.72296ms
+Port 110(pop3) --> Status OPEN ---  Scan takes 121.247065ms
+Port 80(http) --> Status OPEN ---  Scan takes 121.402156ms
+Port 143(imap) --> Status OPEN ---  Scan takes 121.103664ms
+Port 443(https) --> Status OPEN ---  Scan takes 110.957307ms
+Port 587(submission) --> Status OPEN ---  Scan takes 116.198241ms
+Port 993(imaps) --> Status OPEN ---  Scan takes 127.266459ms
+Port 995(pop3s) --> Status OPEN ---  Scan takes 138.238413ms
 ```
+
 ## Support
 
 This scanner currently only support Linux OS.
--- main.go
+++ main.go
@@ -14,17 +14,6 @@ import (
     "github.com/vphatfla/gonet/scanner"
 )
 
-/*	"bufio"
-"fmt"
-"log"
-"net"
-"os"
-
-"github.com/google/gopacket/layers"
-"github.com/google/gopacket/routing"
-routeInfo "github.com/vphatfla/gonet/routing"
-"github.com/vphatfla/gonet/scanner"*/
-
 func main() {
     var boolInt = map[bool]int{
         true: 1,
@@ -131,69 +120,4 @@ func main() {
     if err := cmd.Run(context.Background(), os.Args); err != nil {
         log.Fatal(err)
     }
-    /*
-    log.Println("Welcome to go net")
-
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter IP4 address ")
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        log.Fatal("invalid input : ",err)
-    }
-    input = input[:len(input)-1]
-    ip := net.ParseIP(input).To4()
-    if ip == nil {
-        fmt.Printf("Input must be an IPv4 addr")
-    }
-    router, err := routing.New()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    ri, err := routeInfo.NewRouteInfo(router, ip)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    s, err := scanner.NewScanner(ri, layers.TCPPort(55555))
-    defer s.Close()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    //log.Print("Start scanning well know ports")
-    //res := s.ScanPortsWithRange(ri, layers.TCPPort(0), layers.TCPPort(1023))
-    // log.Println("out at main")
-
-    log.Print("Start scanning all port from 0 to 65535")
-    res := s.ScanPortsWithRange(ri, layers.TCPPort(0), layers.TCPPort(65535))
-    for _, r := range res {
-        log.Println(r)
-    }
-    /*_, err = scanner.ScanWellKnownPorts(ri)
-    log.Print("Done")
-    if err != nil {
-        log.Fatal(err)
-    }
-    /* for r := range len(res) {
-        log.Print(res[r])
-    } */
-
-    /* s, err := scanner.NewScanner(ri, layers.TCPPort(55555))
-    defer s.Close()
-    log.Print("Start scanning")
-    pr, err := s.ScanSinglePort(layers.TCPPort(22))
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Print(pr.ToString())
-
-    /* res, err := s.ScanWellKnownPorts()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for s := range res {
-        log.Println(s)
-    }*/
 }
